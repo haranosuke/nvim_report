978a41843c43f536aa7fe584451e94b08ef0731d
haranosuke <shinnosuke072931o@gmail.com>
2026-01-11 20:57:47 +0900

    Add initial init.lua pure Neovim, no plugin manager

---
+++ nvim/init.lua
@@ -0,0 +1,47 @@
+-- =========================
+-- 基本設定
+-- =========================
+
+vim.opt.encoding = "utf-8"
+
+-- 行番号
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- インデント
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- 検索
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- クリップボード（Windows）
+vim.opt.clipboard = "unnamedplus"
+
+-- =========================
+-- lazy.nvim
+-- =========================
+
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--  vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable",
+--     lazypath,
+--   })
+-- end
+-- 
+-- vim.opt.rtp:prepend(lazypath)
+-- 
+-- require("lazy").setup("plugins")
+
+-- =========================
+-- lazy.nvim end
+-- =========================
